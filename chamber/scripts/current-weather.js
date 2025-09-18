4a38696cb53caf3d4692d8622d441a4cdb812715
jkkitchen <jkitchen1@byupathway.edu>
2025-09-18 13:45:24 -0400

    made some changes to current-weather.js

--- chamber/scripts/current-weather.js
+++ chamber/scripts/current-weather.js
@@ -5,6 +5,9 @@ const weatherDescription = document.querySelector('#weather-description');
 const highTemp = document.querySelector('#high-temp');
 const lowTemp = document.querySelector('#low-temp');
 const humidity = document.querySelector('#humidity');
+const forecastToday = document.querySelector('#forecast-today');
+const forecastTomorrow = document.querySelector('#forecast-next-day');
+const forecastTwoDays = document.querySelector('#forecast-two-days');
 
 //Create variables for the URL
 const myKey = "815579dff197464b572575f1e2216f72";
@@ -12,14 +15,16 @@ const myLat = "34.85";
 const myLong = "-82.40";
 
 //Link to openweathermap.org
-const url = `https://api.openweathermap.org/data/2.5/weather?lat=${myLat}&lon=${myLong}&appid=${myKey}&units=imperial`
+const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${myLat}&lon=${myLong}&appid=${myKey}&units=imperial`;
+const forecastWeatherUrl = `https://api.openweathermap.org/data/3.0/onecall/overview?lat=${myLat}&lon=${myLong}&appid=${myKey}`;
 
-async function apiFetch() {
+//Current Weather Functions
+async function apiFetchCurrentWeather() {
     try {
-        const response = await fetch(url);
+        const response = await fetch(currentWeatherUrl);
         if (response.ok) {
-            const data = await response.json();            
-            displayResults(data);
+            const currentWeatherData = await response.json();            
+            displayCurrentWeatherResults(currentWeatherData);
         } else {
             throw Error(await response.text());
         }
@@ -29,7 +34,7 @@ async function apiFetch() {
 };
 
 //Display the JSON Data onto web page
-function displayResults(data) {          
+function displayCurrentWeatherResults(data) {          
     let desc = data.weather[0].description;
     const iconsrc = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;    
     weatherIcon.setAttribute('src', iconsrc);
@@ -41,5 +46,28 @@ function displayResults(data) {
     humidity.innerHTML = `Humidity: ${data.main.humidity}%`;
 };
 
+//Weather Forecast Functions
+async function apiFetchForecast() {
+    try {
+        const response = await fetch(forecastWeatherUrl);
+        if (response.ok) {
+            const forecastData = await response.json();
+            console.log(forecastData);
+            displayForecastResults(forecastData);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+//Display the JSON Data onto web page
+function displayForecastResults(data) {
+    const forecastToday = data.highTemp;
+};
+
+
 //Run the program
-apiFetch();
+apiFetchCurrentWeather();
+apiFetchForecast();
