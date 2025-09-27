cbb60b97f81ee322e6512372f5596efb91e95336
jkkitchen <jkitchen1@byupathway.edu>
2025-09-27 14:54:37 -0400

    added info to thanks.html

--- chamber/scripts/thanks.js
+++ chamber/scripts/thanks.js
@@ -10,14 +10,17 @@ console.log(formInfo.get('last'));
 console.log(formInfo.get('title'));
 console.log(formInfo.get('email'));
 console.log(formInfo.get('phone'));
-console.log(formInfo.get('business-name'));
+console.log(formInfo.get('organization'));
 console.log(formInfo.get('description'));
 console.log(formInfo.get('membership'));
 console.log(formInfo.get('timestamp'));
 
 //Message on thanks.html
 document.querySelector('#confirmation').innerHTML = `
-<p>Thank you for joining the Greenville Chamber of Commerce.</p>
+<p>Thank you ${formInfo.get('first')} ${formInfo.get('last')} of ${formInfo.get('organization')} for joining the Greenville Chamber of Commerce.</p>
 <p>You are now a ${formInfo.get('membership')} Level Member.</p>
+<p>Phone: ${formInfo.get('phone')}<p>
+<p>E-mail: ${formInfo.get('email')}<p>
+
 <p>${formInfo.get('timestamp')}</p>`;
 
