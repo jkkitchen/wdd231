d28d9d681e391bb8a0f42349094aa416291f49a8
jkkitchen <jkitchen1@byupathway.edu>
2025-10-10 20:17:12 -0400

    added try/catch code

--- project/scripts/fabric.js
+++ project/scripts/fabric.js
@@ -6,65 +6,70 @@ const fabricDisplay = document.querySelector(".fabric-collections");
 
 //Create an async defined function to fetch data from the JSON file using the await/fetch method
 async function getFabricData() {
-    const response = await fetch(fabricFile);
+    //Use try/catch method to handle failures
+    try {    
+        const response = await fetch(fabricFile);
 
-    //Convert the response to a JSON object and store the results
-    const data = await response.json();
+        //Convert the response to a JSON object and store the results
+        const data = await response.json();
 
-    //Store the list/card buttons as variables
-    const allButton = document.querySelector("#all-btn");
-    const fallButton = document.querySelector("#fall-btn");
-    const halloweenButton = document.querySelector("#halloween-btn");
-    const christmasButton = document.querySelector("#christmas-btn");
+        //Store the list/card buttons as variables
+        const allButton = document.querySelector("#all-btn");
+        const fallButton = document.querySelector("#fall-btn");
+        const halloweenButton = document.querySelector("#halloween-btn");
+        const christmasButton = document.querySelector("#christmas-btn");
 
-    //Set starting value for selectedCollections (default is all)
-    const allFabrics = data.fabricCollections
+        //Set starting value for selectedCollections (default is all)
+        const allFabrics = data.fabricCollections
 
-    //Call function that creates the cards for default view
-    displayFabricCollections(allFabrics);
-
-
-
-    //Create click event for 'All' button that displays all fabric collections
-    allButton.addEventListener('click', () => {
-
-        //Display the all collections using the display function
+        //Call function that creates the cards for default view
         displayFabricCollections(allFabrics);
-    });
 
 
 
-    //Create click event for 'Fall' button displays collections with a 'fall' keyword
-    fallButton.addEventListener('click', () => {
+        //Create click event for 'All' button that displays all fabric collections
+        allButton.addEventListener('click', () => {
 
-        //Filter array for only fall collections
-        const fallFabrics = data.fabricCollections.filter(fabric => fabric.keyword === "Fall")
-
-        //Display the fall collection using the display function
-        displayFabricCollections(fallFabrics);
-    });
+            //Display the all collections using the display function
+            displayFabricCollections(allFabrics);
+        });
 
 
 
-    //Create click event for 'Halloween' button displays collections with a 'Halloween' keyword
-    halloweenButton.addEventListener('click', () => {
+        //Create click event for 'Fall' button displays collections with a 'fall' keyword
+        fallButton.addEventListener('click', () => {
 
-        //Filter array for only Halloween collections
-        const halloweenFabrics = data.fabricCollections.filter(fabric => fabric.keyword === "Halloween")
+            //Filter array for only fall collections
+            const fallFabrics = data.fabricCollections.filter(fabric => fabric.keyword === "Fall")
 
-        //Display the fall collection using the display function
-        displayFabricCollections(halloweenFabrics);
-    });
+            //Display the fall collection using the display function
+            displayFabricCollections(fallFabrics);
+        });
 
-    //Create click event for 'Christmas' button displays collections with a 'Christmas' keyword
-    christmasButton.addEventListener('click', () => {
 
-        //Filter array for only Christmas collections
-        const christmasFabrics = data.fabricCollections.filter(fabric => fabric.keyword === "Christmas")
 
-        //Display the fall collection using the display function
-        displayFabricCollections(christmasFabrics);
-    });
+        //Create click event for 'Halloween' button displays collections with a 'Halloween' keyword
+        halloweenButton.addEventListener('click', () => {
+
+            //Filter array for only Halloween collections
+            const halloweenFabrics = data.fabricCollections.filter(fabric => fabric.keyword === "Halloween")
+
+            //Display the fall collection using the display function
+            displayFabricCollections(halloweenFabrics);
+        });
+
+        //Create click event for 'Christmas' button displays collections with a 'Christmas' keyword
+        christmasButton.addEventListener('click', () => {
+
+            //Filter array for only Christmas collections
+            const christmasFabrics = data.fabricCollections.filter(fabric => fabric.keyword === "Christmas")
+
+            //Display the fall collection using the display function
+            displayFabricCollections(christmasFabrics);
+        });
+    } catch (error) {
+        console.error("Failed to fetch fabric data.")
+    };
 };
 
 
